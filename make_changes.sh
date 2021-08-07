9b4c10e7612c1f1b4bca3de760ebadf43ebafaa7
kushal <kushalsai2449@gmail.com>
2021-08-07 19:30:06 +0530

    can edit forms now lol

--- make_changes.sh
+++
@@ -1,4 +0,0 @@
-source ~/PycharmProjects/GUI_is_Cool/venv/bin/activate
-
-python manage.py makemigrations
-python manage.py migrate
--- notes/settings.py
+++ notes/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-with open('/Users/Kushi/PycharmProjects/DontLeaveMeUnfinished/notes/password.txt', 'r') as fp:
-    password = fp.read()
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,12 +76,8 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'notes',
-        'USER': 'root',
-        'PASSWORD': password,
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
